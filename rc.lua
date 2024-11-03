62ae7135d2913b42cd0fa0be5005a231fcb609b0
Thomas Lindop <thomas16256@gmail.com>
2024-11-03 13:10:55 +0000

    >> change help key binding > do more reformatting

--- rc.lua
+++ rc.lua
@@ -249,18 +249,18 @@ root.buttons(gears.table.join(
 -- {{{ Key bindings
 globalkeys = gears.table.join(
     awful.key(
-        { modkey, },  "s",
-             hotkeys_popup.show_help,
+        { modkey, "Shift" },  "/",
+        hotkeys_popup.show_help,
         { description="show help", group="awesome" }
     ),
     awful.key(
         { modkey, },  "Left",
-          awful.tag.viewprev,
+        awful.tag.viewprev,
         { description = "view previous", group = "tag" }
     ),
     awful.key(
         { modkey, },  "Right",
-         awful.tag.viewnext,
+        awful.tag.viewnext,
         { description = "view next", group = "tag" }
     ),
     awful.key(
