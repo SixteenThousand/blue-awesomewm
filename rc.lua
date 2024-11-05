915484a9b5a0640d7f366655517f9fb90949b0c3
Thomas Lindop <thomas16256@gmail.com>
2024-11-05 14:28:12 +0000

    >> change rules > make sure chromium & thunderbird tile > remove unused rules

--- rc.lua
+++ rc.lua
@@ -508,52 +508,22 @@ awful.rules.rules = {
             placement = awful.placement.no_overlap+awful.placement.no_offscreen
         }
     },
-
-    -- Floating clients.
-    {
-        rule_any = {
-            instance = {
-                "DTA",  -- Firefox addon DownThemAll.
-                "copyq",  -- Includes session name in class.
-                "pinentry",
-            },
-            class = {
-                "Arandr",
-                "Gpick",
-                "Kruler",
-                "MessageWin",  -- kalarm.
-                "Sxiv",
-                "Tor Browser", -- Needs a fixed window size to avoid fingerprinting by screen size.
-                "Wpa_gui",
-                "veromix",
-                "xtightvncviewer"
-            },
-
-            -- Note that the name property shown in xprop might be set slightly after creation of the client
-            -- and the name shown there might not match defined rules here.
-            name = {
-                "Event Tester",  -- xev.
-            },
-            role = {
-                "AlarmWindow",  -- Thunderbird's calendar.
-                "ConfigManager",  -- Thunderbird's about:config.
-                -- "pop-up",       -- e.g. Google Chrome's (detached) Developer Tools.
-            },
-        },
-        properties = { floating = true }
-    },
-
-    -- Add titlebars to normal clients and dialogs
+    -- Make dialog windows float
     {
         rule_any = {
             type = { "dialog" }
         },
         properties = { titlebars_enabled = true }
     },
-
-    -- Set Firefox to always map on the tag named "2" on screen 1.
-    -- { rule = { class = "Firefox" },
-    --   properties = { screen = 1, tag = "2" } },
+    -- Make sure  most-everything is tiled by default
+    {
+        rule_any = {
+            instance = { "Thunderbird", "chromium", },
+        },
+        properties = {
+            maximized = false,
+        },
+    },
 }
 -- }}}
 
