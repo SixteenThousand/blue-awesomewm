7b1588683ff96bf146d2ad557fe4b53797bb385c
Thomas Lindop <thomas16256@gmail.com>
2024-11-14 18:11:11 +0000

    >> add alt+tab key bindings

--- rc.lua
+++ rc.lua
@@ -312,7 +312,19 @@ globalkeys = gears.table.join(
         end,
         {description = "focus next by index", group = "client"}
     ),
+    awful.key({ alt }, "Tab",
+        function()
+            awful.client.focus.byidx(1)
+        end,
+        {description = "focus next by index", group = "client"}
+    ),
     awful.key({ super,}, "x",
+        function()
+            awful.client.focus.byidx(-1)
+        end,
+        {description = "focus previous by index", group = "client"}
+    ),
+    awful.key({ alt, "Shift" }, "Tab",
         function()
             awful.client.focus.byidx(-1)
         end,
