f9c4925242c7df827e6b49dae032042a8741bcb2
Thomas Lindop <thomas16256@gmail.com>
2024-11-14 18:02:03 +0000

    >> stop every new window from being the main ("master") window

--- rc.lua
+++ rc.lua
@@ -568,7 +568,8 @@ awful.rules.rules = {
             keys = clientkeys,
             buttons = clientbuttons,
             screen = awful.screen.preferred,
-            placement = awful.placement.no_overlap+awful.placement.no_offscreen
+            placement = awful.placement.no_overlap+awful.placement.no_offscreen,
+            callback = awful.client.setslave,
         }
     },
     -- Make dialog windows float
